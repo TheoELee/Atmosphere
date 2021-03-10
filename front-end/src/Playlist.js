e0fb7c741271b209e964cd60efed7e3a2db345ca
Theo Lee <theo.e.lee@gmail.com>
2021-03-09 17:56:26 -0800

    made playlist a bit prettier

--- front-end/src/Playlist.js
+++ front-end/src/Playlist.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 class Playlist extends Component {
 	constructor(props) {
@@ -11,14 +12,18 @@ class Playlist extends Component {
             token: props.token,
             playlistUri: props.playlistUri,
             tracks: [],
+            artists: [],
+            albums: [],
+            albumArt: []
 		};
     }
 
     // GET 
     getTracksInPlaylist() {
-        const { token, playlistUri, tracks } = this.state;
+        const { token, playlistUri, tracks, artists, albums, albumArt } = this.state;
         const temp = playlistUri.split(":");
         const playlistId = temp[2];
+        const test = [];
 
         fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?market=US`, {
 			method: "GET",
@@ -30,41 +35,106 @@ class Playlist extends Component {
         .then(data => {
             console.log(data)
 
-            data.items.forEach(track => {
-                tracks.push(track.track.name);
+
+            data.items.forEach(item => {
+                tracks.push(item.track.name);
+                artists.push(item.track.artists[0].name);
+                albums.push(item.track.album.name);
+                albumArt.push(item.track.album.images[0].url);
+                // trackData.push(item);
             })
 
             this.setState({
-                tracks: tracks
-            })
-        });
+                tracks: tracks,
+            });
+        })
     }
-
-    // displayTracks() {
-    //     const { tracks } = this.state;
-    //     console.log(tracks);
-    // }
 
     // Need a function that somehow grabs the active track that's playing
 	componentDidMount() {
         this.getTracksInPlaylist()
     }
+
+                    // <Container fluid>
+                    //     <Row>
+                    //         <Col>
+                    //             <Card.Body>{tracks[0].name} - {/*tracks[0].artists[0].name*/} - {/*tracks[0].album.name*/}</Card.Body>
+                    //         </Col>
+                    //     </Row>
+                    //     <Row>
+                    //         <Col>
+                    //         hello
+                    //         </Col>
+                    //     </Row>
+
+                    // </Container>
     
 	render() {
-        const { tracks } = this.state;
+        const { 
+            tracks,
+            artists,
+            albums,
+            albumArt
+         } = this.state;
 		return (
 			<div>
-                A Playlist!
-                <br />
-                ============================
-                <ul>
-                    <li>{tracks[0]}</li>
-                    <li>{tracks[1]}</li>
-                    <li>{tracks[2]}</li>
-                    <li>{tracks[3]}</li>
-                    <li>{tracks[4]}</li>
-                    <li>{tracks[5]}</li>
-                </ul>
+                <Card>
+                    <Card.Header>Atmosphere Weather Playlist</Card.Header>
+                </Card>
+                <Card>
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <Card.Body>1. <img width="50" height="50" src={albumArt[0]}></img> {tracks[0]} - {artists[0]} - {albums[0]}</Card.Body>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Card>
+                <Card>
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <Card.Body>2. <img width="50" height="50" src={albumArt[1]}></img> {tracks[1]} - {artists[1]} - {albums[1]}</Card.Body>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Card>
+                <Card>
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <Card.Body>3. <img width="50" height="50" src={albumArt[2]}></img> {tracks[2]} - {artists[2]} - {albums[2]}</Card.Body>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Card>
+                <Card>
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <Card.Body>4. <img width="50" height="50" src={albumArt[3]}></img> {tracks[3]} - {artists[3]} - {albums[3]}</Card.Body>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Card>
+                <Card>
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <Card.Body>5. <img width="50" height="50" src={albumArt[4]}></img> {tracks[4]} - {artists[4]} - {albums[4]}</Card.Body>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Card>
+                <Card>
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <Card.Body>6. <img width="50" height="50" src={albumArt[6]}></img> {tracks[5]} - {artists[5]} - {albums[5]}</Card.Body>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Card>
 			</div>
 		);
 	}
