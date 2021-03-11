e7103c696865b870ad638a327e6c92b66e03703b
Darika.B <darikadb@gmail.com>
2021-03-10 19:21:35 -0800

    styled playlist

--- front-end/src/Playlist.js
+++ front-end/src/Playlist.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./playlist.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -61,9 +62,9 @@ class Playlist extends Component {
             albumArt
          } = this.state;
 		return (
-			<div>
-                <Card>
-                    <Card.Header>Atmosphere Weather Playlist</Card.Header>
+			<div className="playlist-rows">
+                <Card className="playlist-header">
+                    <Card.Header >Atmosphere Weather Playlist</Card.Header>
                 </Card>
                 <Card>
                     <Container fluid>
