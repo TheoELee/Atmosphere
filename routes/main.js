95fea4aa93138c5f7653f232323c4e91b550f8b0
Theo Lee <theo.e.lee@gmail.com>
2021-03-12 17:52:29 -0800

    changing build paths

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var clientPath = path.join(__dirname + '/../../front-end/build/index.html');
+var clientPath = path.join(__dirname + '/../front-end/build/index.html');
 
 router.get('/', (req, res) => {
   res.sendFile(clientPath);
